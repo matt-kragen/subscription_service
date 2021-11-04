ff4b2c91efb7cd04d7d7c36eac3c6da3e0858162
Matt Kragen <mattdkragen@gmail.com>
2021-11-04 16:53:00 -0700

    Write #create and strong params for subs

---
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -0,0 +1,19 @@
+class Api::V1::SubscriptionsController < ApplicationController
+  def create
+    customer = Customer.find(params[:customer_id])
+    tea = Tea.find(params[:tea_id])
+    subscription = customer.subscriptions.new(subscription_params)
+    if subscription.save
+      render json: subscription
+    else
+      render json: subscription.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def subscription_params
+    params.permit(:tea_id, :customer_id, :title, :price, :status, :frequency)
+  end
+
+end
