698b4173b3a897a59c86b95e252238ed9a9061ea
Matt Kragen <mattdkragen@gmail.com>
2021-11-04 17:21:41 -0700

    Write test for cancelling subscriptions

--- spec/requests/subscriptions_request_spec.rb
+++ spec/requests/subscriptions_request_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe "Subscription requests" do
           tea_id: @tea1.id,
           title: "Tea Subscription",
           price: 5.00,
-          status: "active",
+          status: "Active",
           frequency: 1
         }
         post "/api/v1/customers/#{@customer1.id}/subscriptions", params: subscription_params
@@ -31,6 +31,33 @@ RSpec.describe "Subscription requests" do
         expect(subscription[:data][:attributes]).to have_key(:frequency)
       end
     end
+
+    describe 'PATCH /subscriptions' do
+      it 'can cancel an existing subscription' do
+        subscription_params = {
+          customer_id: @customer1.id,
+          tea_id: @tea1.id,
+          title: "Tea Subscription",
+          price: 5.00,
+          status: "Active",
+          frequency: 1
+        }
+        subscription = Subscription.create(subscription_params)
+
+        patch "/api/v1/customers/#{@customer1.id}/subscriptions/#{subscription.id}", params: {status: "Cancelled"}
+        expect(response).to be_successful
+
+        updated_subscription = JSON.parse(response.body, symbolize_names: true)
+        expect(updated_subscription).to have_key(:data)
+        expect(updated_subscription[:data]).to have_key(:type)
+        expect(updated_subscription[:data]).to have_key(:attributes)
+        expect(updated_subscription[:data][:attributes]).to have_key(:title)
+        expect(updated_subscription[:data][:attributes]).to have_key(:price)
+        expect(updated_subscription[:data][:attributes]).to have_key(:status)
+        expect(updated_subscription[:data][:attributes]).to have_key(:frequency)
+        expect(updated_subscription[:data][:attributes][:status]).to eq("Cancelled")
+      end
+    end
   end
 
   describe 'sad path' do
@@ -41,7 +68,7 @@ RSpec.describe "Subscription requests" do
           tea_id: @tea1.id,
           title: "Tea Subscription",
           price: 5.00,
-          status: "active"
+          status: "Active"
           # Frequency is missing
         }
         post "/api/v1/customers/#{@customer1.id}/subscriptions", params: subscription_params
